af8d911a397b8455079a4978ca7ac8ddf8f7316b
runfourestrun <90913666+runfourestrun@users.noreply.github.com>
2025-07-10 14:10:22 -0400

    fix: resolve PyO3 0.21 compatibility issues in Python bindings\n\n- Remove custom exception classes that conflict with imported types\n- Use PyErr::new with PyValueError for error conversion\n- Clean up unused imports and variables\n- Fix orphan rule violations by using standard Python exceptions\n- Python bindings now build successfully with PyO3 0.21

--- rust/python_bindings/src/lib.rs
+++ rust/python_bindings/src/lib.rs
@@ -6,62 +6,8 @@ use ::cypher_guard::{
     DbSchemaConstraint, DbSchemaIndex, DbSchemaMetadata, DbSchemaProperty,
     DbSchemaRelationshipPattern, PropertyType,
 };
-use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
-
-// === Custom Python Exception Classes ===
-#[pyclass(extends=PyException)]
-pub struct PyCypherGuardError {
-    #[pyo3(get)]
-    pub message: String,
-}
-#[pymethods]
-impl PyCypherGuardError {
-    #[new]
-    fn new(message: String) -> Self {
-        Self { message }
-    }
-}
-
-#[pyclass(extends=PyException)]
-pub struct PyCypherGuardParsingError {
-    #[pyo3(get)]
-    pub message: String,
-}
-#[pymethods]
-impl PyCypherGuardParsingError {
-    #[new]
-    fn new(message: String) -> Self {
-        Self { message }
-    }
-}
-
-#[pyclass(extends=PyException)]
-pub struct PyCypherGuardValidationError {
-    #[pyo3(get)]
-    pub message: String,
-}
-#[pymethods]
-impl PyCypherGuardValidationError {
-    #[new]
-    fn new(message: String) -> Self {
-        Self { message }
-    }
-}
-
-#[pyclass(extends=PyException)]
-pub struct PyCypherGuardSchemaError {
-    #[pyo3(get)]
-    pub message: String,
-}
-#[pymethods]
-impl PyCypherGuardSchemaError {
-    #[new]
-    fn new(message: String) -> Self {
-        Self { message }
-    }
-}
 
 // === Error Conversion Helpers ===
 fn convert_cypher_error(py: Python, err: CypherGuardError) -> PyErr {
@@ -70,28 +16,21 @@ fn convert_cypher_error(py: Python, err: CypherGuardError) -> PyErr {
         CypherGuardError::Validation(e) => convert_validation_error(py, e),
         CypherGuardError::Schema(e) => convert_schema_error(py, e),
         CypherGuardError::InvalidQuery(msg) => {
-            PyErr::from_type(py.get_type::<PyCypherGuardError>(), (msg,))
+            PyErr::new::<pyo3::exceptions::PyValueError, _>(msg)
         }
     }
 }
 
-fn convert_parsing_error(py: Python, err: CypherGuardParsingError) -> PyErr {
-    PyErr::from_type(
-        py.get_type::<PyCypherGuardParsingError>(),
-        (err.to_string(),),
-    )
+fn convert_parsing_error(_py: Python, err: CypherGuardParsingError) -> PyErr {
+    PyErr::new::<pyo3::exceptions::PyValueError, _>(err.to_string())
 }
-fn convert_validation_error(py: Python, err: CypherGuardValidationError) -> PyErr {
-    PyErr::from_type(
-        py.get_type::<PyCypherGuardValidationError>(),
-        (err.to_string(),),
-    )
+
+fn convert_validation_error(_py: Python, err: CypherGuardValidationError) -> PyErr {
+    PyErr::new::<pyo3::exceptions::PyValueError, _>(err.to_string())
 }
-fn convert_schema_error(py: Python, err: CypherGuardSchemaError) -> PyErr {
-    PyErr::from_type(
-        py.get_type::<PyCypherGuardSchemaError>(),
-        (err.to_string(),),
-    )
+
+fn convert_schema_error(_py: Python, err: CypherGuardSchemaError) -> PyErr {
+    PyErr::new::<pyo3::exceptions::PyValueError, _>(err.to_string())
 }
 
 // === Python API Functions ===
@@ -116,26 +55,13 @@ pub fn get_validation_errors_py(
 #[pyfunction]
 pub fn parse_query_py(py: Python, query: &str) -> PyResult<PyObject> {
     match parse_query(query) {
-        Ok(_ast) => Ok(PyDict::new(py).into()),
+        Ok(_ast) => Ok(PyDict::new_bound(py).into()),
         Err(e) => Err(convert_parsing_error(py, e)),
     }
 }
 
 #[pymodule]
-fn cypher_guard(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add("CypherGuardError", py.get_type::<PyCypherGuardError>())?;
-    m.add(
-        "CypherGuardParsingError",
-        py.get_type::<PyCypherGuardParsingError>(),
-    )?;
-    m.add(
-        "CypherGuardValidationError",
-        py.get_type::<PyCypherGuardValidationError>(),
-    )?;
-    m.add(
-        "CypherGuardSchemaError",
-        py.get_type::<PyCypherGuardSchemaError>(),
-    )?;
+fn cypher_guard(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<DbSchema>()?;
     m.add_class::<DbSchemaProperty>()?;
     m.add_class::<PropertyType>()?;
