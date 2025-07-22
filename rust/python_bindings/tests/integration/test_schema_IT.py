7c9124dbfbda8eee05f81e2de6eb1127c32d0dcb
alex <a.gilmore1993@gmail.com>
2025-07-22 08:42:12 -0500

    clean repo, begin adding docstrings

--- rust/python_bindings/tests/integration/test_schema_IT.py
+++ rust/python_bindings/tests/integration/test_schema_IT.py
@@ -1,7 +1,8 @@
 from neo4j_graphrag.schema import get_structured_schema
 from neo4j import Driver
 
-from cypher_guard import DbSchema
+from cypher_guard import DbSchema, validate_cypher, get_validation_errors
+
 def test_load_DbSchema_from_neo4j_graphrag_package(init_data: None, neo4j_driver: Driver):
     schema = get_structured_schema(neo4j_driver, is_enhanced=True)
     assert schema is not None
@@ -18,8 +19,25 @@ def test_load_DbSchema_from_neo4j_graphrag_package(init_data: None, neo4j_driver
     assert len(db_schema.metadata.index) == 1
     assert len(db_schema.metadata.constraint) == 1
     assert len(db_schema.relationships) == 1
+
+def test_validate_cypher_with_schema_from_neo4j_graphrag_package(init_data: None, neo4j_driver: Driver):
+    schema = get_structured_schema(neo4j_driver, is_enhanced=True)
+    db_schema = DbSchema.from_dict(schema)
+
+    query = "MATCH (p:Person) RETURN p.name"
+    result = validate_cypher(query, db_schema)
+    assert result is True
     
     
+def test_get_validation_errors_with_schema_from_neo4j_graphrag_package(init_data: None, neo4j_driver: Driver):
+    schema = get_structured_schema(neo4j_driver, is_enhanced=True)
+    db_schema = DbSchema.from_dict(schema)
+
+    query = "MATCH (p:Person) RETURN p.wrong"
+    result = get_validation_errors(query, db_schema)
+    assert len(result) == 1
+
+
     
     
 
